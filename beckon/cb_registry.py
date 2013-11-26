ff999bac11b11b19e2c31bc3095e46dd7ae5ef02
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2013-11-25 23:07:33 -0500

    add the accept decorator

--- beckon/cb_registry.py
+++ beckon/cb_registry.py
@@ -4,12 +4,32 @@ from collections import defaultdict
 
 
 class CallbackRegistry(object):
+    """
+    The CallbackRegistry is the dispatcher in the callback system.
+
+    Registrations are shared between multiple instances of a given CallbackRegistry.
+    There's no need to pass the instance around, just create a new one wherever needed.
+    """
 
     _registrations = defaultdict(list)
 
+    def accept(self, msg):
+        def decorator(f):
+            def inner(*args):
+                return f(*args)
+            self.register_cb(msg, inner)
+            return inner
+        return decorator
+
     def register_cb(self, msg, fn):
+        """
+        Register a new callback function for a given message key
+        """
         self._registrations[msg].append(fn)
 
     def cb(self, msg, params):
+        """
+        Launches the callback chain for a given message key
+        """
         for fn in self._registrations[msg]:
             fn(params)
--- beckon/tests/test_cb_registry.py
+++ beckon/tests/test_cb_registry.py
@@ -8,6 +8,9 @@ from unittest import TestCase
 
 class TestCallbackRegistry(TestCase):
 
+    def setUp(self):
+        CallbackRegistry._registrations.clear()
+
     def test_call(self):
         reg = CallbackRegistry()
         fn = Mock()
@@ -17,3 +20,28 @@ class TestCallbackRegistry(TestCase):
         reg.cb(sentinel.msg, sentinel.param)
 
         fn.assert_called_once_with(sentinel.param)
+
+    def test_shared_registrations(self):
+        reg1 = CallbackRegistry()
+        fn = Mock()
+
+        reg1.register_cb(sentinel.msg, fn)
+
+        del reg1
+
+        reg2 = CallbackRegistry()
+        reg2.cb(sentinel.msg, sentinel.param)
+
+        fn.assert_called_once_with(sentinel.param)
+
+    def test_accept(self):
+        reg = CallbackRegistry()
+
+        class CalledException(Exception):
+            pass
+
+        @reg.accept(sentinel.msg)
+        def fn(params):
+            raise CalledException()
+
+        self.assertRaises(CalledException, reg.cb, sentinel.msg, sentinel.param)
